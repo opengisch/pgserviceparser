cd02864f33ecfab05c5ac2739b1cffbdceb7e26f
signedav <david@opengis.ch>
2022-02-10 17:11:29 +0100

    fix topic

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/opengisch/pgserviceparser",
     classifiers=[
-        'Topic :: PostgreSQL',
+        'Topic :: Database',
         'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
