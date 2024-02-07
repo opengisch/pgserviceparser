4a304617a709a07c2e2490bfdce03defb37fb0d8
Julien <dev@ingeoveritas.com>
2024-02-07 18:30:16 +0100

    Complete type hints (#6)
    * complete docstring
    
    * tooling: ignore .venv
    
    * formatting: blacking code
    
    * meta: add type hints
    
    ---------
    
    Co-authored-by: Denis Rouzaud <denis.rouzaud@gmail.com>

--- pgserviceparser/__init__.py
+++ pgserviceparser/__init__.py
@@ -1,5 +1,7 @@
 import configparser
 import os
+from typing import List, Optional
+
 
 
 def conf_path() -> str:
@@ -16,7 +18,7 @@ def conf_path() -> str:
         pg_config_path = "~/.pg_service.conf"
     return os.path.expanduser(pg_config_path)
 
-def full_config(conf_file_path: str = None) -> configparser.ConfigParser:
+def full_config(conf_file_path: Optional[str] = None) -> configparser.ConfigParser:
     """Returns full pgservice config as configparser.ConfigParser().
 
     :param str conf_file_path: path to configuration file to load. If None the `conf_path` is used, defaults to None
@@ -32,7 +34,7 @@ def full_config(conf_file_path: str = None) -> configparser.ConfigParser:
     return config
 
 
-def service_config(service_name: str, conf_file_path: str = None) -> dict:
+def service_config(service_name: str, conf_file_path: Optional[str] = None) -> dict:
     """Returns the config from the given service name as a dict.
 
     :param str service_name: service name
@@ -46,7 +48,10 @@ def service_config(service_name: str, conf_file_path: str = None) -> dict:
     return {}
 
 def write_service_setting(
-    service_name: str, setting_key: str, setting_value: str, conf_file_path: str = None
+    service_name: str,
+    setting_key: str,
+    setting_value: str,
+    conf_file_path: Optional[str] = None,
 ) -> bool:
     """Returns true if it could write the setting to the file.
 
@@ -57,6 +62,7 @@ def write_service_setting(
 
     :return bool: True if the setting has been successfully written
     """
+
     config = full_config(conf_file_path)
     if service_name in config:
         config[service_name][setting_key] = setting_value
@@ -66,7 +72,7 @@ def write_service_setting(
     return False
 
 
-def service_names(conf_file_path: str = None) -> list:
+def service_names(conf_file_path: Optional[str] = None) -> List[str]:
     """Returns all service names in a list.
 
     :param str conf_file_path: path to the pg_service.conf. If None the `conf_path()` is used, defaults to None
