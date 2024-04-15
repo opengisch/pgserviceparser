ca9b66345300aa0d0a2a019e84b38a2fe03b15df
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-04-15 18:41:18 +0200

    raise exceptions rather than returning false/empty

--- pgserviceparser/__init__.py
+++ pgserviceparser/__init__.py
@@ -1,9 +1,10 @@
 import configparser
 from os import getenv, path
+from pathlib import Path
 from typing import Optional
 
 
-def conf_path() -> str:
+def conf_path() -> Path:
     """Returns the path found for the pg_service.conf on the system as string.
 
     Returns:
@@ -16,10 +17,10 @@ def conf_path() -> str:
         pg_config_path = path.join(getenv("PGSYSCONFDIR"), "pg_service.conf")
     else:
         pg_config_path = "~/.pg_service.conf"
-    return path.expanduser(pg_config_path)
+    return Path(pg_config_path).expanduser()
 
 
-def full_config(conf_file_path: Optional[str] = None) -> configparser.ConfigParser:
+def full_config(conf_file_path: Optional[Path] = None) -> configparser.ConfigParser:
     """Returns full pgservice config as configparser.ConfigParser().
 
     Args:
@@ -27,17 +28,22 @@ def full_config(conf_file_path: Optional[str] = None) -> configparser.ConfigPars
 
     Returns:
         pg services loaded as ConfigParser
+
+    Raises:
+        ServiceFileNotFound: when the service file is not found
     """
     if conf_file_path is None:
         conf_file_path = conf_path()
 
+    if not conf_file_path.exists():
+        raise ServiceFileNotFound
+
     config = configparser.ConfigParser()
-    if path.exists(conf_file_path):
-        config.read(conf_file_path)
+    config.read(conf_file_path)
     return config
 
 
-def service_config(service_name: str, conf_file_path: Optional[str] = None) -> dict:
+def service_config(service_name: str, conf_file_path: Optional[Path] = None) -> dict:
     """Returns the config from the given service name as a dict.
 
     Args:
@@ -46,11 +52,16 @@ def service_config(service_name: str, conf_file_path: Optional[str] = None) -> d
 
     Returns:
         service settings as dictionary
+
+    Raises:
+        ServiceNotFound: when the service is not found
     """
     config = full_config(conf_file_path)
-    if service_name in config:
-        return dict(config[service_name])
-    return {}
+
+    if service_name not in config:
+        raise ServiceNotFound
+
+    return dict(config[service_name])
 
 
 def write_service_setting(
@@ -58,50 +69,52 @@ def write_service_setting(
     setting_key: str,
     setting_value: str,
     conf_file_path: Optional[str] = None,
-) -> bool:
-    """Returns true if it could write the setting to the file.
+):
+    """Writes a service setting to the service file.
 
     Args:
-        service_name: service's name
+        service_name: service name
         setting_key: key
         setting_value: value
         conf_file_path: path to the pg_service.conf. If None the `conf_path()` is used, defaults to None
 
-    Returns:
-        True if the setting has been successfully written
+    Raises:
+        ServiceFileNotFound: when the service file is not found
+        ServiceNotFound: when the service is not found
     """
 
     config = full_config(conf_file_path)
-    if service_name in config:
-        config[service_name][setting_key] = setting_value
-        with open(conf_file_path or conf_path(), "w") as configfile:
-            config.write(configfile)
-            return True
-    return False
+    if service_name not in config:
+        raise ServiceNotFound(service_name)
+
+    config[service_name][setting_key] = setting_value
+    with open(conf_file_path or conf_path(), "w") as configfile:
+        config.write(configfile)
 
 
 def write_service(
     service_name: str,
     settings: dict,
     conf_file_path: Optional[str] = None,
-) -> bool:
-    """Returns true if it could write the settings to the file.
+):
+    """Writes a complete service to the service file.
 
     Args:
-        service_name: service's name
+        service_name: service name
         settings: settings dict defining the service config
         conf_file_path: path to the pg_service.conf. If None the `conf_path()` is used, defaults to None
 
-    Returns:
-        True if the setting has been successfully written
+    Raises:
+        ServiceFileNotFound: when the service file is not found
+        ServiceNotFound: when the service is not found
     """
     config = full_config(conf_file_path)
-    if service_name in config:
-        config[service_name] = settings.copy()
-        with open(conf_file_path or conf_path(), "w") as configfile:
-            config.write(configfile)
-            return True
-    return False
+    if service_name not in config:
+        raise ServiceNotFound(service_name)
+
+    config[service_name] = settings.copy()
+    with open(conf_file_path or conf_path(), "w") as configfile:
+        config.write(configfile)
 
 
 def service_names(conf_file_path: Optional[str] = None) -> list[str]:
@@ -112,6 +125,18 @@ def service_names(conf_file_path: Optional[str] = None) -> list[str]:
 
     Returns:
         list of every service registered
+
+    Raises:
+        ServiceFileNotFound: when the service file is not found
     """
+
     config = full_config(conf_file_path)
     return config.sections()
+
+
+class ServiceNotFound(Exception):
+    pass
+
+
+class ServiceFileNotFound(Exception):
+    pass
--- test/test_lib.py
+++ test/test_lib.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 import os
+import shutil
 import unittest
 from pathlib import Path
 
 from pgserviceparser import (
+    ServiceNotFound,
     conf_path,
     service_config,
     service_names,
@@ -16,16 +18,20 @@ PGSERVICEPARSER_SRC_PATH = Path(os.environ["PGSERVICEPARSER_SRC_DIR"])
 
 class TestLib(unittest.TestCase):
     def setUp(self):
-        os.environ["PGSERVICEFILE"] = str(PGSERVICEPARSER_SRC_PATH / "test" / "data" / "service_base.txt")
+        service_file_path_base = PGSERVICEPARSER_SRC_PATH / "test" / "data" / "service_base.txt"
+        service_file_path = PGSERVICEPARSER_SRC_PATH / "test" / "data" / "pgservice.conf"
+        os.environ["PGSERVICEFILE"] = str(service_file_path)
+
+        shutil.copy(service_file_path_base, service_file_path)
 
     def test_conf_path(self):
-        self.assertEqual(os.environ["PGSERVICEFILE"], conf_path())
+        self.assertEqual(Path(os.environ["PGSERVICEFILE"]), conf_path())
 
     def test_service_names(self):
         self.assertEqual(service_names(), ["service_1", "service_2", "service_3"])
 
     def test_service_config(self):
-        self.assertEqual({}, service_config("non_existing_service"))
+        self.assertRaises(ServiceNotFound, service_config, "non_existing_service")
 
         conf = service_config("service_1")
         self.assertEqual(
@@ -33,21 +39,21 @@ class TestLib(unittest.TestCase):
         )
 
     def test_write_service_setting(self):
-        self.assertFalse(write_service_setting("non_existing_service", "key", "value"))
+        self.assertRaises(ServiceNotFound, write_service_setting, "non_existing_service", "key", "value")
 
         # Create setting
-        self.assertTrue(write_service_setting("service_1", "new_key", "new_value"))
+        write_service_setting("service_1", "new_key", "new_value")
         conf = service_config("service_1")
         self.assertIn("new_key", conf)
         self.assertEqual(conf["new_key"], "new_value")
 
         # Overwrite setting
-        self.assertTrue(write_service_setting("service_1", "port", "1"))
+        write_service_setting("service_1", "port", "1")
         conf = service_config("service_1")
         self.assertEqual(conf["port"], "1")
 
     def test_write_service(self):
-        self.assertFalse(write_service("non_existing_service", {"key": "value"}))
+        self.assertRaises(ServiceNotFound, write_service, "non_existing_service", {"key": "value"})
 
         # Overwrite the whole service_3 config using service_2 params
         config_3 = service_config("service_3")
@@ -55,7 +61,7 @@ class TestLib(unittest.TestCase):
             config_3, {"host": "host_3", "dbname": "db_3", "port": "3333", "user": "user_3", "password": "pwd_3"}
         )
         config_2 = service_config("service_2")
-        self.assertTrue(write_service("service_3", config_2))
+        write_service("service_3", config_2)
         self.assertEqual(service_config("service_3"), config_2)
 
     def tearDown(self):
